667476ac4549c52bd74c1ea8eaf7ab1cb79ccbf2
Thuy Tran <christiesunnie@gmail.com>
2024-06-19 14:18:54 -0400

    Initial set up client and server, code forma tting configuration and connect client and server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function HomePage() {
-  return <div>Hello World</div>;
+  const [homePageMsg, setHomePageMsg] = useState("");
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/", {
+        headers: {},
+      })
+      .then((res) => {
+        const data = res.data;
+        setHomePageMsg(`${data.message} My name is ${data.name} :)`);
+      })
+      .catch((error) => console.log(error));
+  });
+
+  return (
+    <div>
+      <h1>{homePageMsg}</h1>
+    </div>
+  );
 }
 
 export default HomePage;
